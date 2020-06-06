be302f2d85886ee9bdb34118e6dc0e769e3eff96
nivasinijeykumar <56480524+nivasinijeykumar@users.noreply.github.com>
2020-06-06 15:22:59 +0530

    Add files via upload

---
+++ admin/sidebar.php
@@ -0,0 +1,56 @@
+<div class="sidebar" data-color="white" data-active-color="danger">
+      <!--
+        Tip 1: You can change the color of the sidebar using: data-color="blue | green | orange | red | yellow"
+    -->
+      <div class="logo">
+          <div class="logo-image-small">
+            <img src="../images/bibliologo.png">
+          </div>
+        </a>
+        
+          <!-- <div class="logo-image-big">
+            <img src="../assets/img/logo-big.png">
+          </div> -->
+    
+      </div>
+      <div class="sidebar-wrapper">
+        <ul class="nav">
+          <li>
+            <a href="./dashboard.php">
+              <i class="nc-icon nc-bank"></i>
+              <p>Dashboard</p>
+            </a>
+          </li>
+          <li>
+            <a href="./category.php">
+              <i class="nc-icon nc-diamond"></i>
+              <p>Category</p>
+            </a>
+          </li>
+          <li>
+            <a href="./books.php">
+              <i class="nc-icon nc-pin-3"></i>
+              <p>Books</p>
+            </a>
+          </li>
+          <li>
+            <a href="./bookings.php">
+              <i class="nc-icon nc-bell-55"></i>
+              <p>Bookings</p>
+            </a>
+          </li>
+          <!-- <li class="active ">
+            <a href="./contact.php">
+              <i class="nc-icon nc-single-02"></i>
+              <p>Contacts</p>
+            </a>
+          </li> -->
+          <li>
+            <a href="./contact.php">
+              <i class="nc-icon nc-tile-56"></i>
+              <p>Feedback</p>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
